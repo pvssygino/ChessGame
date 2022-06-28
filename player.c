5018e2337b80085f7f374bc4b2c228461d5e32d7
gino <lorenzo.rondinelli48@gmail.com>
2022-06-28 13:06:32 +0200

    Update player.c

--- player.c
+++ player.c
@@ -1,4 +1,5 @@
 #include "player.h"
+#include "script.h"
 #include <stdio.h>
 #include <stdlib.h>
 in_player initialize(int p) {
@@ -32,23 +33,50 @@ int find(in_player list, int n_pezzo) {
 	return done;
 }
 int can_move(int MAT[8][8], int pezzo, int current_x, int current_y, int dest_x, int dest_y) {
-	bool miopezzo = false;
-	int check = 0;
-	if (pezzo == 200 + PEDONE_N || pezzo == 100 + PEDONE_N) {
-		if (current_y == dest_y) {
-			if ((abs(current_x - dest_x) == 1 || abs(current_x - dest_x) == 2)) {  //movimento verticale pedone
-				if (MAT[dest_x][dest_y] == VUOTO) check = 1;
+	int can_do = 0;
+	bool miopezzo = mio(MAT, dest_x, dest_y, pezzo);
+	bool check_diagonale = false;
+
+	if (miopezzo == false) {
+		if (pezzo == 200 + PEDONE_N || pezzo == 100 + PEDONE_N) {
+			if (current_y == dest_y) {
+				if ((abs(current_x - dest_x) == 1 || abs(current_x - dest_x) == 2)) {  //movimento verticale pedone
+					if (MAT[dest_x][dest_y] == VUOTO) can_do = 1;
+				}
+			}
+			else if (abs(current_y - dest_y) == 1 && abs(current_x - dest_x) == 1) { //movimento diagonale pedone
+				if (MAT[dest_x][dest_y] != VUOTO) can_do = 1;
 			}
 		}
-		else if (abs(current_y - dest_y) == 1 && abs(current_x - dest_x) == 1) { //movimento diagonale pedone
-			if (MAT[dest_x][dest_y] != VUOTO) check = 1;
+
+		if (pezzo == 200 + ALFIERE_N || pezzo == 100 + ALFIERE_N) {
+			for (int i = 1; i < 8 - current_y; i++) {
+				if (dest_x == current_x - i && dest_y == current_y + i)
+					for (int j = 1; j < 8 - current_y; j++) {
+							check_diagonale = mio(MAT, current_x - j, current_y + j, pezzo);
+							if (check_diagonale == true) break;
+							else can_do = 1;
+					}
+				else if (dest_x == current_x - i && dest_y == current_y - i) 
+					for (int j = 1; j < 8 - current_y; j++) {
+						check_diagonale = mio(MAT, current_x - j, current_y - j, pezzo);
+						if (check_diagonale == true) break;
+						else can_do = 1;
+					}
+				else if (dest_x == current_x + i && dest_y == current_y - i) 
+					for (int j = 1; j < 8 - current_y; j++) {
+						check_diagonale = mio(MAT, current_x + j, current_y - j, pezzo);
+						if (check_diagonale == true) break;
+						else can_do = 1;
+					}
+				else if (dest_x == current_x + i && dest_y == current_y + i) 
+					for (int j = 1; j < 8 - current_y; j++) {
+						check_diagonale = mio(MAT, current_x + j, current_y + j, pezzo);
+						if (check_diagonale == true) break;
+						else can_do = 1;
+					}
+			}
 		}
-		
 	}
-	if (pezzo == 200 + ALFIERE_N || pezzo == 100 + ALFIERE_N) {
-		
-
-
-	}
-	return check;
+	return can_do;
 }
